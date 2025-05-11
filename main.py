5b91a4831fd70d9c71e0d5046bc19fa29c4cffee
Tashif Ahmad Khan <tashifkhan010@gmail.com>
2025-05-12 03:41:52 +0530

    import form .env

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+import pandas as pd
+import os
+import dotenv
+
+dotenv.load_dotenv()
+expoit_id = os.getenv("EXPLOIT_ID")
+expoit_pass = os.getenv("EXPLOIT_PASS")
+
 def main():
     print("Hello from sophos-brute!")
 
